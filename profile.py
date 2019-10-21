2eb4dfcab3bce4ee8ebd03474d4becb1b9b90f07
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-21 05:58:45 -0400

    reload

--- profile.py
+++ profile.py
@@ -1,32 +1,19 @@
-# Import the Portal object.
 import geni.portal as portal
-# Import the ProtoGENI library.
 import geni.rspec.pg as pg
 import geni.rspec.igext as IG
 
-# Create a portal context.
 pc = portal.Context()
-
-# Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
-
-# Lists for the nodes and such
-nodeList = []
 
 tourDescription = \
 """
-This profile provides the template for blah blah installed on Ubuntu 18.04
+This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client who is to be authenticated using LDAP. 
 """
 
-#
-# Setup the Tour info with the above description and instructions.
-#  
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
-
 prefixForIP = "192.168.1."
-
 link = request.LAN("lan")
 
 for i in range(2):
@@ -34,10 +21,7 @@ for i in range(2):
     node = request.XenVM("ldapserver")    
   else:
     node = request.XenVM("ldapclient")
-  
-  node.cores = 4
-  node.ram = 4096
-  
+   
   node.routable_control_ip = "true"  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
@@ -52,13 +36,13 @@ for i in range(2):
   else:
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/clientSide_ldap.sh"))
     node.addService(pg.Execute(shell="sh", command="/local/repository/clientSide_ldap.sh"))
-  
+    
   # Following 4 lines would work with ldap_install_combined.sh (lines 33-38 do the same thing)
   #node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ldap_install_combined.sh"))
   #node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ldap_install.sh"))
   #node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/clientSide_ldap.sh"))
   #node.addService(pg.Execute(shell="sh", command="/local/repository/ldap_install_combined.sh"))
-  
+
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
 
