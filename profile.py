0ff2bda0f3fe7865c8e5f63d4327ab5baef28383
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-21 05:35:27 -0400

    copy good lines?

--- profile.py
+++ profile.py
@@ -1,21 +1,38 @@
+# Import the Portal object.
 import geni.portal as portal
+# Import the ProtoGENI library.
 import geni.rspec.pg as pg
 import geni.rspec.igext as IG
 
+# Create a portal context.
 pc = portal.Context()
+
+#pc.defineParameter( "n", "Number of worker nodes, a number from 2 to 5", portal.ParameterType.INTEGER, 4 )
+#params = pc.bindParameters()
+
+# Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
+# Lists for the nodes and such
 nodeList = []
 
 tourDescription = \
 """
-This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client who is to be authenticated using LDAP. 
+This profile provides the template for  blah blah installed on Ubuntu 18.04
 """
 
+#
+# Setup the Tour info with the above description and instructions.
+#  
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
+
 prefixForIP = "192.168.1."
+
+#maxSize = 5
+#beegfnNum = params.n + 1
+
 link = request.LAN("lan")
 
 for i in range(2):
@@ -29,6 +46,7 @@ for i in range(2):
   
   node.routable_control_ip = "true"  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  
   iface = node.addInterface("if" + str(i+1))
   iface.component_id = "eth"+ str(i+1)
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
