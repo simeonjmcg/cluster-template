eea61757c36d525570d2ef5702170a5746a7a201
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-21 05:38:18 -0400

    python tabs

--- profile.py
+++ profile.py
@@ -30,9 +30,6 @@ request.addTour(tour)
 
 prefixForIP = "192.168.1."
 
-#maxSize = 5
-#beegfnNum = params.n + 1
-
 link = request.LAN("lan")
 
 for i in range(2):
@@ -46,7 +43,6 @@ for i in range(2):
   
   node.routable_control_ip = "true"  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-  
   iface = node.addInterface("if" + str(i+1))
   iface.component_id = "eth"+ str(i+1)
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
@@ -59,12 +55,12 @@ for i in range(2):
   else:
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/clientSide_ldap.sh"))
     node.addService(pg.Execute(shell="sh", command="/local/repository/clientSide_ldap.sh"))
-    
+  
   # Following 4 lines would work with ldap_install_combined.sh (lines 33-38 do the same thing)
   #node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ldap_install_combined.sh"))
   #node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ldap_install.sh"))
   #node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/clientSide_ldap.sh"))
   #node.addService(pg.Execute(shell="sh", command="/local/repository/ldap_install_combined.sh"))
-
+  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
