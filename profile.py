6da351b0ebfb72038f5549d7baed1d72b66562ae
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-21 05:31:03 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,6 +5,8 @@ import geni.rspec.igext as IG
 pc = portal.Context()
 request = pc.makeRequestRSpec()
 
+nodeList = []
+
 tourDescription = \
 """
 This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client who is to be authenticated using LDAP. 
@@ -27,7 +29,7 @@ for i in range(2):
   
   node.routable_control_ip = "true"  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-  iface = node.addInterface("if" + str(i))
+  iface = node.addInterface("if" + str(i+1))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
